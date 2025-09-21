28f0d3865002ef4eef41ca8af339588ffd149ecd
Nate Babyak <nate.babyak@outlook.com>
2025-09-20 22:29:55 -0400

    feat: added card component. added insight snippet. updated data table

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 	import AppSidebar from '$lib/components/layout/app-sidebar.svelte';
-	import DataTable from './data-table.svelte';
+	import * as Card from '$lib/components/ui/card/index.js';
 	import { columns } from './columns.js';
+	import DataTable from './data-table.svelte';
 	import type { PageProps } from './$types';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 
 	let { data }: PageProps = $props();
 </script>
@@ -11,6 +12,21 @@
 <svelte:head>
 	<title>Dashboard - JobAt</title>
 </svelte:head>
+
+{#snippet insightCard()}
+	<Card.Root>
+		<Card.Header>
+			<Card.Title>Title</Card.Title>
+			<Card.Description>Description</Card.Description>
+		</Card.Header>
+		<Card.Content>
+			<p>Card Content</p>
+		</Card.Content>
+		<Card.Footer>
+			<p>Card Footer</p>
+		</Card.Footer>
+	</Card.Root>
+{/snippet}
 
 <Sidebar.Provider>
 	<AppSidebar />
