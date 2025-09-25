c1930586cd804defb84383547bc3bd8124a2d30f
Nate Babyak <nate.babyak@outlook.com>
2025-09-24 20:07:31 -0400

    feat: added applications schema. updated users schema.

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,16 +1,24 @@
-import { pgTable, integer, text, timestamp } from 'drizzle-orm/pg-core';
+import { integer, pgTable, serial, text, timestamp, varchar } from 'drizzle-orm/pg-core';
 
-export const users = pgTable('user', {
-	id: text('id').primaryKey(),
-	githubId: integer('github_id'),
-	username: text('username'),
-	googleId: text('google_id'),
-	name: text('name'),
-	email: text('email'),
-	passwordHash: text('password_hash')
+const timestamps = {
+	createdAt: timestamp('created_at').defaultNow().notNull(),
+	updatedAt: timestamp('updated_at')
+};
+
+export const applications = pgTable('applications', {
+	position: varchar('position'),
+	company: varchar('company'),
+	submittedOn: timestamp('submitted_on', { mode: 'date', withTimezone: true }),
+	screenedOn: timestamp('screening_on', { mode: 'date', withTimezone: true }),
+	interviewingOn: timestamp('interviewing_on', { mode: 'date', withTimezone: true }),
+	offeredOn: timestamp('offered_on', { mode: 'date', withTimezone: true }),
+	acceptedOn: timestamp('accepted_on', { mode: 'date', withTimezone: true }),
+	rejectedOn: timestamp('rejected_on', { mode: 'date', withTimezone: true }),
+	withdrawnOn: timestamp('withdrawn_on', { mode: 'date', withTimezone: true }),
+	...timestamps
 });
 
-export const sessions = pgTable('session', {
+export const sessions = pgTable('sessions', {
 	id: text('id').primaryKey(),
 	userId: text('user_id')
 		.notNull()
@@ -18,6 +26,17 @@ export const sessions = pgTable('session', {
 	expiresAt: timestamp('expires_at', { withTimezone: true, mode: 'date' }).notNull()
 });
 
-export type Session = typeof sessions.$inferSelect;
+export const users = pgTable('users', {
+	id: serial('id').primaryKey(),
+	emailAddress: text('email_address').unique(),
+	passwordHash: text('password_hash'),
+	githubId: integer('github_id').unique(),
+	githubUsername: text('github_username'),
+	googleId: text('google_id').unique(),
+	googleUserame: text('google_username'),
+	...timestamps
+});
 
+export type Application = typeof applications.$inferSelect;
+export type Session = typeof sessions.$inferSelect;
 export type User = typeof users.$inferSelect;
--- src/lib/server/db/users.ts
+++
@@ -1,6 +0,0 @@
-import { pgTable } from 'drizzle-orm/pg-core';
-import { db } from './index';
-
-export async function getUserFromGitHubId() {
-	const user = await db.select().from(users);
-}
