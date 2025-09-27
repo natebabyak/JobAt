c5ec0d8943cac06dbe1d775e8fff839be58ea09b
Nate Babyak <nate.babyak@outlook.com>
2025-09-26 21:41:31 -0400

    feat: updated application schema

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -12,35 +12,36 @@ const applicationLevel = pgEnum('application_level', [
 	'fellow'
 ]);
 
-const applicationLocation = pgEnum('application_location', ['on-site', 'hybrid', 'remote']);
+const applicationLocationType = pgEnum('application_location_type', ['hybrid', 'onsite', 'remote']);
 
-const timestamps = {
-	createdAt: timestamp('created_at').defaultNow().notNull(),
-	updatedAt: timestamp('updated_at').defaultNow().notNull(),
-	deletedAt: timestamp('deleted_at')
-};
+export const skills = pgTable('skills', {});
 
 export const applications = pgTable('applications', {
 	id: serial('id').primaryKey(),
 	userId: serial('user_id')
 		.notNull()
 		.references(() => users.id),
-	position: varchar('position').notNull(),
+	title: varchar('title').notNull(),
 	company: varchar('company').notNull(),
 	submittedOn: timestamp('submitted_on', { mode: 'date', withTimezone: true }),
-	screenedOn: timestamp('screening_on', { mode: 'date', withTimezone: true }),
-	interviewingOn: timestamp('interviewing_on', { mode: 'date', withTimezone: true }),
+	preInterviewOn: timestamp('pre_interview_on', { mode: 'date', withTimezone: true }),
+	interviewOn: timestamp('interview_on', { mode: 'date', withTimezone: true }),
 	offeredOn: timestamp('offered_on', { mode: 'date', withTimezone: true }),
 	acceptedOn: timestamp('accepted_on', { mode: 'date', withTimezone: true }),
 	rejectedOn: timestamp('rejected_on', { mode: 'date', withTimezone: true }),
 	withdrawnOn: timestamp('withdrawn_on', { mode: 'date', withTimezone: true }),
-	canonicalizedCity: varchar('canonicalized_city'),
-	normalizedPosition: varchar('normalized_position'),
-	normalizedCompany: varchar('normalized_company'),
-	level: applicationLevel('level'),
-	location: applicationLocation('location'),
-	skills: varchar().array().default([]),
-	...timestamps
+	normalizedTitle: varchar('normalized_title'),
+	canonicalizedCompany: varchar('normalized_company'),
+	seniority: applicationLevel('level'),
+	locationType: applicationLocationType(),
+	skills: varchar('skills').array().default([]),
+	city: varchar('city'),
+	state: varchar('state'),
+	country: varchar('country'),
+	notes: text('notes'),
+	createdAt: timestamp('created_at', { mode: 'date', withTimezone: true }).defaultNow().notNull(),
+	updatedAt: timestamp('updated_at', { mode: 'date', withTimezone: true }).defaultNow().notNull(),
+	deletedAt: timestamp('deleted_at', { mode: 'date', withTimezone: true })
 });
 
 export const sessions = pgTable('sessions', {
@@ -53,13 +54,14 @@ export const sessions = pgTable('sessions', {
 
 export const users = pgTable('users', {
 	id: serial('id').primaryKey(),
-	emailAddress: text('email_address').unique(),
-	passwordHash: text('password_hash'),
+	emailAddress: varchar('email_address').unique(),
+	passwordHash: varchar('password_hash'),
 	githubId: integer('github_id').unique(),
 	githubUsername: text('github_username'),
 	googleId: text('google_id').unique(),
 	googleUserame: text('google_username'),
-	...timestamps
+	createdAt: timestamp('created_at', { mode: 'date', withTimezone: true }).defaultNow().notNull(),
+	updatedAt: timestamp('updated_at', { mode: 'date', withTimezone: true }).defaultNow().notNull()
 });
 
 export type Application = typeof applications.$inferSelect;
