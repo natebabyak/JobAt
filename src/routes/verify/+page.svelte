aa4a405562bc27dd1fa31bd98e2207aa24b41ad5
Nate Babyak <nate.babyak@outlook.com>
2025-10-02 18:32:52 -0400

    added temporary backend boilerplate and otp component from shadcn-svelte

---
+++ src/routes/verify/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import * as InputOTP from '$lib/components/ui/input-otp/index.js';
+	import * as Card from '$lib/components/ui/card/index.js';
+	import { AtSign } from '@lucide/svelte';
+	import { REGEXP_ONLY_DIGITS } from 'bits-ui';
+</script>
+
+<main class="flex h-screen items-center">
+	<Card.Root class="mx-auto w-full max-w-sm">
+		<Card.Content>
+			<InputOTP.Root maxlength={6} pattern={REGEXP_ONLY_DIGITS}>
+				{#snippet children({ cells })}
+					<InputOTP.Group>
+						{#each cells.slice(0, 3) as cell}
+							<InputOTP.Slot {cell} />
+						{/each}
+					</InputOTP.Group>
+					<InputOTP.Separator />
+					<InputOTP.Group>
+						{#each cells.slice(3, 6) as cell}
+							<InputOTP.Slot {cell} />
+						{/each}
+					</InputOTP.Group>
+				{/snippet}
+			</InputOTP.Root>
+		</Card.Content>
+	</Card.Root>
+</main>
